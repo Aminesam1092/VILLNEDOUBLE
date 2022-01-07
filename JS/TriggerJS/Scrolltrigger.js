4b7c0a1b66e0f2d55003b3e247677ae0a8690acb
Aminesam1092 <doublaliamin13@gmail.com>
2022-01-07 16:21:16 +0900

    Recommit

--- JS/TriggerJS/Scrolltrigger.js
+++ JS/TriggerJS/Scrolltrigger.js
@@ -21,3 +21,25 @@ gsap.to(".OURSTORY__Catchcopy",{
     duraion: 5,
     opacity: 1,
 });
+
+gsap.to(".OURSTORYconteiner__Twocontents",{
+    scrollTrigger: {
+        trigger: ".OURSTORYconteiner__Twocontents",
+        start: "",
+        end: '+=1000',
+        scrub: true,
+    },
+    duraion: 10,
+    opacity: 1,
+});
+
+gsap.to(".OURSTORYconteiner__Brandeintroductory",{
+    scrollTrigger: {
+        trigger: ".OURSTORYconteiner__Brandeintroductory",
+        start: "",
+        end: '+=1000',
+        scrub: true,
+    },
+    duraion: 5,
+    opacity: 1,
+});
